71c4e8117c49d6afbfbf93b1100909ac11eda249
Hemasri <hemasribandari@gmail.com>
2025-08-15 16:39:11 +0530

    Temporary changes for Render

--- db_config.py
+++ db_config.py
@@ -1,11 +1,11 @@
-import pymysql
+# import pymysql
 
-# Database connection
-conn = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="",  # Default is empty in XAMPP
-    database="ride_connect"
-)
-cursor = conn.cursor()
-print("Database connected successfully!")
+# # Database connection
+# conn = pymysql.connect(
+#     host="localhost",
+#     user="root",
+#     password="",  # Default is empty in XAMPP
+#     database="ride_connect"
+# )
+# cursor = conn.cursor()
+# print("Database connected successfully!")
